c1f1a27d57edbde7f8d47d381362cfd4521df47c
cookavsky <cookavsky@gmail.com>
2019-03-24 23:46:09 +0100

    -Remove html files and add js function hide show stage, mode, home - For left make text, for right background in sticky position

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-const HomeUT = document.querySelectorAll(".Home-UT-Intro");
-const HomeUTLeft = document.querySelectorAll(".Home-UT-Left");
-const HomeUTRight = document.querySelectorAll(".Home-UT-Right");
+const Menu = document.querySelectorAll(".nav-menu-list");
 
-const changeBG = () => {
-/// Change BG Home UT ///
-    for(let h1L = 0; h1L < HomeUTLeft.length; h1L++) {
-        HomeUTLeft[h1L].addEventListener("click", () => {
-            if (HomeUT[h1L].className === "Home-UT-Intro Home-UT-3") {
-                HomeUT[0].classList.remove("Home-UT-3");
-                HomeUT[0].classList.add("Home-UT-2");
-            } else if (HomeUT[0].className === "Home-UT-Intro Home-UT-2") {
-                HomeUT[0].classList.remove("Home-UT-2");
-                HomeUT[0].classList.add("Home-UT-1");
-            }
-        });
-    } for(let h1R = 0; h1R < HomeUTRight.length; h1R++) {
-        HomeUTRight[h1R].addEventListener("click", () => {
-            if (HomeUT[0].className === "Home-UT-Intro Home-UT-1") {
-                HomeUT[0].classList.remove("Home-UT-1");
-                HomeUT[0].classList.add("Home-UT-2");
-            } else if (HomeUT[0].className === "Home-UT-Intro Home-UT-2") {
-                HomeUT[0].classList.remove("Home-UT-2");
-                HomeUT[0].classList.add("Home-UT-3");
+const changeMenu = () => {
+    for(let i = 0; i < Menu.length; i++) {
+        Menu[i].addEventListener("click", () => {
+            switch(Menu[i]) {
+                case Menu[0]:
+                    Menu[0].classList.add("list-activ");
+                    Menu[1].classList.remove("list-activ");
+                   Menu[2].classList.remove("list-activ");
+                   break;
+                case Menu[1]:
+                    Menu[1].classList.add("list-activ");
+                    Menu[0].classList.remove("list-activ");
+                   Menu[2].classList.remove("list-activ");
+                   break;
+                case Menu[2]:
+                    Menu[2].classList.add("list-activ");
+                    Menu[0].classList.remove("list-activ");
+                   Menu[1].classList.remove("list-activ");
+                   break;
+                default:
+                    break;
             }
         });
     }
 };
 
-changeBG();
+changeMenu();
